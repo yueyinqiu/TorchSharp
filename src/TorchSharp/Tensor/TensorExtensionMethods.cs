6e8333c1884869743a9526d73d32e4d5d3ee956d
Stefan-Alin Pahontu <56953855+alinpahontu2912@users.noreply.github.com>
2024-11-27 12:11:14 +0100

    test newline

--- src/TorchSharp/Tensor/TensorExtensionMethods.cs
+++ src/TorchSharp/Tensor/TensorExtensionMethods.cs
@@ -238,7 +238,7 @@ namespace TorchSharp
         ///
         /// Primarily intended for use in interactive notebooks.
         /// </remarks>
-        public static string cstr(this Tensor tensor, string? fltFormat = "g5", int? width = 100, string? newLine = "\n", CultureInfo? cultureInfo = null)
+        public static string cstr(this Tensor tensor, string? fltFormat = "g5", int? width = 100, string? newLine = null, CultureInfo? cultureInfo = null)
         {
             return tensor.ToString(TensorStringStyle.CSharp, fltFormat, width, cultureInfo, newLine);
         }
